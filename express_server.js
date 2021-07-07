e4ba6f31e0e20e5850b7458de6fc0bbe4a3e3ecb
jan-lab <tnlatte@outlook.com>
2021-07-06 21:52:48 -0700

    add a Edit button

--- express_server.js
+++ express_server.js
@@ -8,7 +8,7 @@ const morgan = require('morgan');
 app.use(morgan('dev'));
 
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
 
 
 const generateRandomString = () => {
@@ -56,16 +56,8 @@ app.get("/urls/new", (req, res) => {
 //Read GET /urls/:shortURL
 app.get("/urls/:shortURL", (req, res) => {
   const urlID = req.params.shortURL;
-  const templateVars = { shortURL: urlID, longURL: urlDatabase[req.params.shortURL] };
+  const templateVars = { shortURL: urlID, longURL: urlDatabase[req.params.shortURL] }; //1:31:03
   res.render("urls_show", templateVars);
-});
-
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  const shortURL = generateRandomString();
-  urlDatabase[shortURL] = req.body.longURL;
-  //console.log(urlDatabase);
-  res.redirect(`/urls/${shortURL}`);
 });
 
 app.get("/u/:shortURL", (req, res) => {
@@ -73,6 +65,29 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+//Add Post /urls
+app.post("/urls", (req, res) => {
+  //console.log(req.body);  // Log the POST request body to the console
+  const newURL = req.body;
+  const id = generateRandomString();
+  //urlDatabase[id] = req.body.longURL;
+  urlDatabase[id] = newURL.longURL;
+  //console.log(urlDatabase);
+  res.redirect(`/urls/${id}`);
+});
+
+//Edit Post /urls/:shortURL
+app.post('/urls/:shortURL', (req, res) => {
+  const shortURL = req.params.shortURL;
+  //grab the body of the request
+  const newURL = req.body.newURL;
+  //console.log(body); //if you see req.body is undefined, you don't have the body-parser set up.
+  urlDatabase[shortURL] = newURL;
+  //once we are done with whatever it is, we are going back to the homepage.
+  res.redirect('/urls');
+});
+
+//Delete Post /urls/:id/delete
 //Add a POST route that removes a URL resource: POST /urls/:shortURL/delete
 //After the resource has been deleted, redirect the client back to the urls_index page ("/urls").
 app.post("/urls/:shortURL/delete", (req, res) => {
