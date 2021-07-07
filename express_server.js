b62affd995478c03db0134fbd4e3d25b2be1ec6d
jan-lab <tnlatte@outlook.com>
2021-07-06 18:28:11 -0700

    add a Delete button

--- express_server.js
+++ express_server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.set("view engine", "ejs"); //tells the Express app to use EJS as its templating engine
+app.set("view engine", "ejs"); //tells the Express app to use EJS as its templating engine //ejs automatically look into templates inside a folder Views.
 
-//const morgan = require('morgan');
-//app.use(morgan('dev'));
+const morgan = require('morgan');
+app.use(morgan('dev'));
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -21,19 +21,19 @@ const generateRandomString = () => {
   return result;
 };
 
+//in-memory database
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+
+//home page
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
-
+//json of database
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
@@ -42,18 +42,21 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-////
+//Browse GET /urls
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
+  const templateVars = { urls: urlDatabase }; //Q. how does it work here when urls: urlDatabase is entered?? why is it not just {urlDatabase}?
+  res.render("urls_index", templateVars);   //renders a template named urls_index.
 });
+
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+//Read GET /urls/:shortURL
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const urlID = req.params.shortURL;
+  const templateVars = { shortURL: urlID, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
@@ -62,19 +65,22 @@ app.post("/urls", (req, res) => {
   const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
   //console.log(urlDatabase);
-  res.redirect(`/urls/:${shortURL}`);
+  res.redirect(`/urls/${shortURL}`);
 });
-
 
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
+//Add a POST route that removes a URL resource: POST /urls/:shortURL/delete
+//After the resource has been deleted, redirect the client back to the urls_index page ("/urls").
+app.post("/urls/:shortURL/delete", (req, res) => {
+  const id = req.params.shortURL;
+  delete urlDatabase[id];
+  res.redirect('/urls');
+});
 
-
-
-
-
-
-
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
