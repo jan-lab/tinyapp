fdb32d44b81ee3092086dccf51223006ea02b178
jan-lab <tnlatte@outlook.com>
2021-07-08 23:19:47 -0700

    cleanup 23:19

--- express_server.js
+++ express_server.js
@@ -3,11 +3,10 @@ const express = require("express");
 const morgan = require('morgan');
 const bcrypt = require('bcryptjs');
 const { getUserByEmail } = require('./helpers.js');
-//const { getUserByEmail } = require('./helpers.js');
 const app = express();
 const PORT = 8080; // default port 8080
 
-app.set("view engine", "ejs"); //tells the Express app to use EJS as its templating engine; ejs automatically look into templates inside a folder Views.
+app.set("view engine", "ejs"); //tells the Express app to use EJS as its templating engine
 app.use(morgan('dev'));
 //const bodyParser = require("body-parser");
 app.use(express.urlencoded({extended: true}));
@@ -60,43 +59,15 @@ const urlDatabase = {
 };
 
 const users = {
-  // "apple": {
-  //   id: "apple",
-  //   email: "a@a.com",
-  //   password: "1234" //because this cannot be compared with hashed password and then login would not work, I've commented out exisgint data.
-  // },
-  // "green": {
-  //   id: "green",
-  //   email: "b@b.com",
-  //   password: "1234"
-  // },
-  // "stone": {
-  //   id: "stone",
-  //   email: "c@c.com",
-  //   password: "1234"
-  //}
 };
 
 //messages to use
 const tellsVisitorToLogin = "Please log in <a href='/login'><b>here</b></a>";
 const informsRestriction = "The URL does not belong to you. Click <a href='/urls'><b>here</b></a> for the list of your URLs";
 
-// //returns a user of an email if in the users database
-// const findUserByEmail = (email) => {
-//   for (const userId in users) {
-//     const user = users[userId];
-//     if (user.email === email) {
-//       return user;
-//     }
-//   }
-//   return null;
-// };
-
-
 
 //GET /
 app.get("/", (req, res) => {
-  // if (!req.session.user_id) {
   if (!req.session.user_id) {
     
     return res.redirect('/login');
@@ -104,47 +75,23 @@ app.get("/", (req, res) => {
   return res.redirect('/urls');
 });
 
-// //GET /urls.json
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
-
-//GET /hello
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
-
-
 //GET /urls
 app.get("/urls", (req, res) => {
-  //if (!req.session.user_id) {
   if (!req.session.user_id) {
     return res.send(tellsVisitorToLogin);
-    //return res.redirect('/login');
   }
     
-  console.log(req.session);
   let user;
-  // for (let key in users) {
-  //   if (key === req.session.user_id) {
-  //     user = users[key];
-  //   }
-  // }
-
   if (req.session.user_id) {
-    //const email = req.body.email;
     const email = req.session.email;
-    //console.log(req.body.email); //undefined
     user = getUserByEmail(email, users);
-    //console.log(user); //null
-    //console.log(users); // correctly showing updated database
   }
 
   const urls = urlsForUser(req.session.user_id);
   
-  const templateVars = { urls, user }; //the key names, urls and user, (which also are values in this case) are the variable names we assign.
+  const templateVars = { urls, user };
 
-  res.render("urls_index", templateVars);   //renders a template named urls_index and makes templateVars available to the template.
+  res.render("urls_index", templateVars);
 });
 
 
@@ -152,17 +99,9 @@ app.get("/urls", (req, res) => {
 app.get("/urls/new", (req, res) => {
   let user;
 
-  //if (!req.session.user_id) {
   if (!req.session.user_id) {
     return res.redirect('/login');
   }
-  
-  // for (let key in users) {
-  //   //if (key === req.session.user_id) {
-  //   if (key === req.session.user_id) {
-  //     user = users[key];
-  //   }
-  // }
 
   if (req.session.user_id) {
     const email = req.session.email;
@@ -188,11 +127,7 @@ app.get("/urls/:shortURL", (req, res) => {
   const urlID = req.params.shortURL;
 
   let user;
-  // for (let userKey in users) {
-  //   if (userKey === req.session.user_id) {
-  //     user = users[userKey];
-  //   }
-  // }
+
   if (req.session.user_id) {
     const email = req.session.email;
     user = getUserByEmail(email, users);
@@ -214,30 +149,6 @@ app.get("/u/:shortURL", (req, res) => {
   res.status('404').send('URL does not exist');
 });
 
-// //GET /register //working version prior to removing users db
-// app.get('/register', (req,res) => {
-//   let user;
-//   for (let userKey in users) {
-//     if (userKey === req.session.user_id) {
-//       user = users[userKey];
-//     }
-//   }
-//   const templateVars = {user};
-//   res.render('register', templateVars);
-// });
-
-// //returns a user of an email if in the users database
-// const findUserByEmail = (email, usersDatabase) => {
-//   const users = usersDatabase;
-//   for (const userId in users) {
-//     const user = users[userId];
-//     if (user.email === email) {
-//       return user;
-//     }
-//   }
-//   return null;
-// };
-
 //GET /register
 app.get('/register', (req,res) => {
   let user;
@@ -253,11 +164,7 @@ app.get('/register', (req,res) => {
 //GET /login
 app.get('/login', (req,res) => {
   let user;
-  // for (let userKey in users) {
-  //   if (userKey === req.session.user_id) {
-  //     user = users[userKey];
-  //   }
-  // }
+
   if (req.session.user_id) {
     const email = req.session.email;
     user = getUserByEmail(email, users);
@@ -287,21 +194,15 @@ app.post('/register', (req,res) => {
   // hash the user's password
   bcrypt.genSalt(10, (err, salt) => {
     bcrypt.hash(password, salt, (err, hash) => {
-      //const users = {};
       users[id] = {
         id,
         email,
         password: hash
       };
       
-      //console.log(password)
-
-      //set a user_id cookie containing the user's newly generated ID.
-      //res.cookie('user_id', id);
       req.session.user_id = id;
       req.session.email = req.body.email;
-      console.log('POST /login',req.body.email);
-      //console.log(users); //correctly stored
+      
       return res.redirect('/urls');
 
     });
@@ -320,28 +221,20 @@ app.post('/login', (req,res) => {
   }
   // find out if email is already in use
   const user = getUserByEmail(email, users);
-  // console.log(password); //1234
-  // console.log(user.password); //hashed
   if (!user) {
     return res.status(403).send(`Email Not Found - <html><body>You can create an account <a href='/register'><b>here</b></a>.</body></html>\n`);
   }
 
-  // we found the user! now we need to compare the password //shouldn't have saved email and password to cookie
+  // user found - compare the password
   bcrypt.compare(password, user.password, (err, result) => {
-    //console.log(result) //true
-    // console.log(password); //1234
-    // console.log(user.password); //hashed
     
     if (!result) {
       return res.status(403).send('Incorrect Password');
     }
 
-    // happy path! at last
-    //res.cookie('user_id', user.id);
-    //req.session.userId = user.id;
+    //Happy path
     req.session.user_id = user.id;
     req.session.email = user.email;
-    //console.log('POST /login',user.email);
 
     // redirect the user
     res.redirect('/urls');
@@ -382,7 +275,7 @@ app.post('/urls/:shortURL', (req, res) => {
   const shortURL = req.params.shortURL;
   //grab the body of the request
   const newURL = req.body.newURL;
-  //console.log(body); //if you see req.body is undefined, you don't have the body-parser set up.
+ 
   urlDatabase[shortURL].longURL = newURL;
 
   res.redirect('/urls');
@@ -408,8 +301,6 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 //POST /logout
 app.post('/logout', (req,res) => {
   //clear the cookie
-  //res.clearCookie('user_id');
-  //console.log(req.cookies);
   req.session = null;
   return res.redirect('/urls');
 });
--- helpers.js
+++ helpers.js
@@ -11,4 +11,24 @@ const getUserByEmail = (email, usersDatabase) => {
   return undefined; //changed this from null to undefined due to mocha test requirement
 };
 
-module.exports = { getUserByEmail };
+//generates a random string to create a short URL
+const generateRandomString = () => {
+  let result           = '';
+  const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
+
+//messages to use
+const tellsVisitorToLogin = "Please log in <a href='/login'><b>here</b></a>";
+const informsRestriction = "The URL does not belong to you. Click <a href='/urls'><b>here</b></a> for the list of your URLs";
+
+module.exports = {
+  getUserByEmail,
+  generateRandomString,
+  tellsVisitorToLogin,
+  informsRestriction}
+;
